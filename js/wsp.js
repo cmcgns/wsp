0c32a32efe1e24c3b62c95d6171fcc87e49e1e39
cmcgns <82063601+cmcgns@users.noreply.github.com>
2021-12-04 05:52:42 +0100

    Add files via upload

---
+++ js/wsp.js
@@ -0,0 +1,22 @@
+﻿var ratio = 1.0;
+
+function on_resize() 
+{
+    if ($(window).width() / $(window).height() < ratio) 
+    {
+        $(".stretch_img").addClass("stretch_img_height");
+    }
+    else 
+    {
+        $(".stretch_img").removeClass("stretch_img_height");
+    }
+}
+
+$(document).ready(function () {
+    var w = $('.stretch_img').attr('width');
+    var h = $('.stretch_img').attr('height');
+    ratio = w / h;
+
+    on_resize();
+    $(window).bind('resize', on_resize);
+});
